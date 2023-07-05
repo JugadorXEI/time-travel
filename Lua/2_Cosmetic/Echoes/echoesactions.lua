c0243e07e5acf9274cf5ae94c93ced38beb12f89
JugadorXEI <eugeniom9@gmail.com>
2023-07-05 09:00:03 +0200

    Removed the super() calls, added the code from hardcode directly as super() was causing warnings

--- Lua/2_Cosmetic/Echoes/echoesactions.lua
+++ Lua/2_Cosmetic/Echoes/echoesactions.lua
@@ -3,81 +3,106 @@ local ECHOES_ACTIONS_VERSION = 1
 -- avoid redefiniton on updates
 if timetravel.ECHOES_ACTIONS_VERSION == nil or timetravel.ECHOES_ACTIONS_VERSION < ECHOES_ACTIONS_VERSION then
 
+local MF2_FIRING = MF2_FIRING
+local MF2_SUPERFIRE = MF2_SUPERFIRE
+local S_StartSound = S_StartSound
+local sfx_mario2 = sfx_mario2
+
 -- Action overrides for sounds.
 function A_PlaySound(actor, var1, var2)
-	super(actor, var1, var2)
+	if leveltime < 2 then return end
+	if not var1 then return end
+	
+	local soundTarget = nil
+	if var2 then soundTarget = actor end
+	S_StartSound(soundTarget, var1)
+
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if leveltime < 2 and var2 >> 16 then return end
-	local whichOne = linkedItem
-	if not (var2 & 65535) then whichOne = nil end
-
-	S_StartSound(whichOne, var1)
+	if var2 then S_StartSound(linkedItem, var1) end
 end
 
 function A_Pain(actor, var1, var2)
-	super(actor, var1, var2)
+	local painSound = actor.info.painsound
+	
+	actor.flags2 = $ & (~MF2_FIRING)
+	actor.flags2 = $ & (~MF2_SUPERFIRE)
+
+	if not painSound then return end
+	S_StartSound(actor, painSound)
+
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if actor.info.painsound then
-		S_StartSound(linkedItem, actor.info.painsound)
-	end
+	S_StartSound(linkedItem, painSound)
 end
 
 function A_PlayActiveSound(actor, var1, var2)
-	super(actor, var1, var2)
+	local activeSound = actor.info.activesound
+	if not activeSound then return end
+	
+	S_StartSound(actor, activeSound)
+	
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if actor.info.activesound then
-		S_StartSound(linkedItem, actor.info.activesound)
-	end
+	S_StartSound(linkedItem, activeSound)
 end
 
 function A_PlayAttackSound(actor, var1, var2)
-	super(actor, var1, var2)
+	local attackSound = actor.info.attacksound
+	if not attackSound then return end
+	
+	S_StartSound(actor, attackSound)
+	
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if actor.info.attacksound then
-		S_StartSound(linkedItem, actor.info.attacksound)
-	end
+	S_StartSound(linkedItem, attackSound)
 end
 
 function A_PlaySeeSound(actor, var1, var2)
-	super(actor, var1, var2)
+	local seeSound = actor.info.seesound
+	if not seeSound then return end
+	
+	S_StartSound(actor, seeSound)
+	
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if actor.info.seesound then
-		S_StartSound(linkedItem, actor.info.seesound)
-	end
+	S_StartSound(linkedItem, seeSound)
 end
 
 function A_Scream(actor, var1, var2)
-	super(actor, var1, var2)
+	local screamSound = nil
+
+	local deathSound = actor.info.deathsound
+	local tracerType = nil
+	if actor.tracer then tracerType = actor.tracer.type end
+	
+	if tracerType == MT_SHELL or tracerType == MT_FIREBALL then screamSound = sfx_mario2
+	elseif deathSound then screamSound = deathSound end
+	
+	if not screamSound then return end
+	S_StartSound(actor, screamSound)
+	
 	if not timetravel.isActive then return end
 	
 	local linkedItem = actor.linkedItem
 	if linkedItem == nil then return end
 	
-	if actor.tracer and (actor.tracer.type == MT_SHELL or actor.tracer.type == MT_FIREBALL) then
-		S_StartSound(linkedItem, sfx_mario2)
-	elseif actor.info.deathsound then
-		S_StartSound(linkedItem, actor.info.deathsound)
-	end
+	S_StartSound(actor, screamSound)
 end
 -- End.
 
