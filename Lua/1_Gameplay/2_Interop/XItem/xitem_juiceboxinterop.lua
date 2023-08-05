a1f7bbeef708e1e943f276d332a598436ae50433
JugadorXEI <eugeniom9@gmail.com>
2023-08-05 21:10:43 +0200

    Move player arrow fix to xitem_playerarrowinterop.lua, also fix player arrow bug from Friendmod there

--- Lua/1_Gameplay/2_Interop/XItem/xitem_juiceboxinterop.lua
+++ Lua/1_Gameplay/2_Interop/XItem/xitem_juiceboxinterop.lua
@@ -1,14 +1,11 @@
 -- The optimization momento certificado.
-local KITEM_SNEAKER = KITEM_SNEAKER
-local TICRATE = TICRATE
-local k_itemblink = k_itemblink
 local k_itemamount = k_itemamount
 local min = min
 -- No more opti.
 
 local xitemHooked = false
 
-local VERSION = 2
+local VERSION = 3
 local JB_NAMESPACE = "JUICEBOX"
 
 local function xitemHandler()
@@ -27,28 +24,6 @@ local function xitemHandler()
 	{
 		lib = "By Tyron - XItem interop by JugadorXEI",
 		ver = VERSION,
-		-- Fixes XItem stealing the Player Arrow references creating an infinite loop when Juicebox loads them back.
-		playerArrowSpawn = function(arrowMo, playerMo)
-			if G_BattleGametype() then return end
-			if not (JUICEBOX and JUICEBOX.value) then return end
-			local player = playerMo.player
-			if not player then return end
-
-			local f = P_SpawnMobj(arrowMo.x, arrowMo.y, arrowMo.z, MT_XITEMPLAYERARROW)
-			f.threshold = arrowMo.threshold
-			f.movecount = arrowMo.movecount
-			f.flags = arrowMo.flags
-			f.flags2 = arrowMo.flags2
-			f.target = arrowMo.target
-			f.scale = arrowMo.scale
-			f.destscale = arrowMo.destscale
-			f.state = arrowMo.state	
-			player.JBspy = f
-			
-			-- P_KillMobj(arrowMo) -- Let the hook handle this.
-			-- This results in two MT_XITEMPLAYERARROWs being created but fuck it we ball.
-			return true
-		end
 	})
 	
 	-- This NEEDS to have a function BECAUSE OTHERWISE HOOKS WON'T WORK
