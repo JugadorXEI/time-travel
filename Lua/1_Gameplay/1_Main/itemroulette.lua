a98994319014c2ef74360c84cefc6359c723a26d
JugadorXEI <eugeniom9@gmail.com>
2024-02-05 19:48:07 +0100

    Fix players going into the air if they were on a different Z on the subframe between PreThink and Think, also make the code more consistent with the rest

--- Lua/1_Gameplay/1_Main/itemroulette.lua
+++ Lua/1_Gameplay/1_Main/itemroulette.lua
@@ -1,3 +1,8 @@
+--[[
+GPLv2 notice: Reimplementation of K_KartItemRoulette,
+made 05/02/2023 (dd/mm/aaaa).
+]]
+
 local ROULETTE_VERSION = 12
 
 -- avoid redefiniton on updates
@@ -5,11 +10,14 @@ if timetravel.ROULETTE_VERSION == nil or timetravel.ROULETTE_VERSION < ROULETTE_
 
 local k_itemroulette = k_itemroulette
 local k_position = k_position
+local TICRATE = TICRATE
+local BT_ATTACK = BT_ATTACK
 
 local ROULETTE_ENDTIC = TICRATE*3
 
 local function isValidItemOddsPlayer(player)
-	return player.valid and not player.spectator and player.mo and player.mo.valid or player.mo.timetravel == nil
+	local playerMo = player.mo
+	return player.valid and not player.spectator and playerMo and playerMo.valid or playerMo.timetravelconsts == nil
 end
 
 addHook("PreThinkFrame", function()
@@ -31,13 +39,12 @@ addHook("PreThinkFrame", function()
 		roulettetic = $ + 1 -- the function +1s the var inside, we simulate it.
 		
 		local pingame = 0
-		for p in players.iterate
+		for p in players.iterate do
 			if p.spectator then continue end
 			pingame = $ + 1
 		end
 		
 		local roulettestop = TICRATE + (3*(pingame - playerPosition))
-		print(roulettestop)
 		local canbemashed = roulettestop <= roulettetic
 		
 		if not (roulettetic >= ROULETTE_ENDTIC or (canbemashed and player.cmd.buttons & BT_ATTACK)) then continue end
@@ -46,14 +53,14 @@ addHook("PreThinkFrame", function()
 			if otherPlayer == player or not isValidItemOddsPlayer(otherPlayer) then continue end
 			
 			-- Two people mashed or rolled at the same time, non-sense time.
-			if otherPlayer.itemRollComeBack then continue end
+			if otherPlayer.timetravelconsts.itemRollComeBack then continue end
 			local otherPlayerPosition = otherPlayer.kartstuff[k_position]
 			
 			if playerPosition > otherPlayerPosition then
 				if (player.mo.timetravel.isTimeWarped ~= otherPlayer.mo.timetravel.isTimeWarped) then
+					otherPlayer.timetravelconsts.itemRollComeBack = true
+					otherPlayer.timetravelconsts.storedComeBackZ = otherPlayer.mo.z
 					timetravel.changePositions(otherPlayer.mo, true)
-					otherPlayer.itemRollComeBack = true
-					print(string.format("Player %s has been teleported (position %d)", otherPlayer.name, otherPlayerPosition))
 				end
 			end
 		end
@@ -65,9 +72,21 @@ addHook("ThinkFrame", function(player)
 	if not timetravel.isActive then return end
 	
 	for player in players.iterate do
-		if player.itemRollComeBack then
-			timetravel.changePositions(player.mo, true)
-			player.itemRollComeBack = false
+		if player.timetravelconsts.itemRollComeBack then
+			local playerMo = player.mo
+			
+			timetravel.changePositions(playerMo, true)
+			
+			local didZActuallyChange = player.timetravelconsts.storedComeBackZ ~= playerMo.z
+			playerMo.z = player.timetravelconsts.storedComeBackZ
+			
+			-- Prevents camera weirdness.
+			if didZActuallyChange and timetravel.isDisplayPlayer(player) ~= -1 then
+				COM_BufInsertText(consoleplayer, "resetcamera")
+			end
+			
+			player.timetravelconsts.itemRollComeBack = false
+			player.timetravelconsts.storedComeBackZ = 0
 		end
 	end
 end)
@@ -79,7 +98,7 @@ addHook("ShouldSquish", function(target, inflictor, source)
 	-- We're looking for inflictor-less, source-less squishing.
 	if inflictor ~= nil and source ~= nil then return nil end
 	-- Prevent people from getting crushed on that one cut.
-	if target.itemRollComeBack then return false end
+	if target.timetravelconsts.itemRollComeBack then return false end
 end)
 
 timetravel.ROULETTE_VERSION = ROULETTE_VERSION
